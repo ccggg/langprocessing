bf3fb2d94cc7782515cf0495d8379dc56ec5af66
ccggg <connor.gaskell@go.edgehill.ac.uk>
2017-06-12 17:02:33 +0100

    Add files via upload

--- SciKit Learn/Review Analysis/ReviewAnalysis.py
+++ SciKit Learn/Review Analysis/ReviewAnalysis.py
@@ -1,9 +1,12 @@
 # http://scikit-learn.org/stable/tutorial/text_analytics/working_with_text_data.html#tutorial-setup
 
 import nltk
+import numpy as np
+from sklearn.pipeline import Pipeline
 from sklearn.datasets import load_files
 from sklearn.metrics import confusion_matrix
 from sklearn.naive_bayes import MultinomialNB
+from sklearn.linear_model import SGDClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 
@@ -11,21 +14,29 @@ from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 filedir = r'G:\Python\DataSets\movie_reviews'
 
 # Load the dataset from the file diretory to be used as training data
-movie_train = load_files(filedir, shuffle=True)
+movie_train = load_files(filedir, shuffle = True, random_state = 19)
 
-movie_vec = CountVectorizer(min_df=2, tokenizer=nltk.word_tokenize)
+# Convert a collection of text documents to a matrix of token counts
+movie_vec = CountVectorizer(min_df = 2, tokenizer = nltk.word_tokenize)
 movie_counts = movie_vec.fit_transform(movie_train.data)
 
 tfidf_transformer = TfidfTransformer()
 movie_tfidf = tfidf_transformer.fit_transform(movie_counts)
-docs_train, docs_test, y_train, y_test = train_test_split(movie_tfidf, movie_train.target, test_size = 0.20, random_state = 12)
 
-clf = MultinomialNB().fit(docs_train, y_train)
+clf = MultinomialNB().fit(movie_tfidf, movie_train.target)
 
-reviews_new = ["This movie was excellent.",
-               "Absolute joy ride.",
+reviews_new = ["It was a decent movie.",
+               "I loved the movie.",
                "The movie was pretty poor.",
-               ""]
+               "Bad.",
+               "Eh.",
+               "Could have been better.",
+               "Good.",
+               "Not good.",
+               "Best film I have ever seen.",
+               "4/10",
+               "7/10",
+               "Nice."]
 
 reviews_new_counts = movie_vec.transform(reviews_new)
 reviews_new_tfidf = tfidf_transformer.transform(reviews_new_counts)
@@ -33,4 +44,6 @@ reviews_new_tfidf = tfidf_transformer.transform(reviews_new_counts)
 pred = clf.predict(reviews_new_tfidf)
 
 for review, category in zip(reviews_new, pred):
-    print('%r => %s' % (review, movie_train.target_names[category]))
+    print review
+    print "This is a", movie_train.target_names[category], "review."
+    print ""
